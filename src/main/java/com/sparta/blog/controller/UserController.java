24875b7f8af7f07771bd88095be1238fe5a4f986
SuengHyunLee <dltmdgus2000@naver.com>
2023-08-04 18:05:59 +0900

    Spring Security 로그인

--- src/main/java/com/sparta/blog/controller/UserController.java
+++ src/main/java/com/sparta/blog/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     public UserController(UserService userService) {
+
         this.userService = userService;
     }
 
@@ -22,11 +23,13 @@ public class UserController {
 
     @GetMapping("/user/login-page")
     public String loginPage() {
+
         return "login";
     }
 
     @GetMapping("/user/signup")
     public String signupPage() {
+
         return "signup";
     }
 
